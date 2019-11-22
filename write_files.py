89b56177c222cd1b57169b62b78d1f5680f839f3
Adam Knowles <acknowles524@gmail.com>
2019-11-21 21:20:20 -0600

    changed to names.csv

--- write_files.py
+++ write_files.py
@@ -4,10 +4,10 @@ with open('names.csv', mode='w') as csv_file:
     fieldnames = ['first_name', 'last_name', 'email']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
-    # writer.writeheader()
+    writer.writeheader()
     writer.writerow({'first_name': 'John', 'last_name': 'Smith', 'email': 'November@november'})
     writer.writerow({'first_name': 'Erica', 'last_name': 'Knowles', 'email': 'March@november'})
-    writer.writerow({'first_name': 'Adam', 'last_name': 'Yentzer', 'email': 'May@may' })
-    writer.writerow({'first_name': 'Jeremy', 'last_name': 'Davis', 'email': 'January@january' })
-    writer.writerow({'first_name': 'Diana', 'last_name': 'Davis', 'email': 'January@january' })
+    writer.writerow({'first_name': 'Adam', 'last_name': 'Yentzer', 'email': 'May@may'})
+    writer.writerow({'first_name': 'Jeremy', 'last_name': 'Davis', 'email': 'January@january'})
+    writer.writerow({'first_name': 'Diana', 'last_name': 'Davis', 'email': 'January@january'})
 
